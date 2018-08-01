1e40e9fe0cd3813c88f294143e5585184d540722
sozoev <sozoev@gmail.com>
2018-08-01 22:29:37 +0600

    Renamed Money::Money class to BaseMoney, created class methods franc and dollar

--- lib/money/dollar.rb
+++ lib/money/dollar.rb
@@ -1,29 +1,36 @@
-module Money
-
-  class Money
-    # @param [Integer] amount
-    def initialize(amount)
-      @amount = amount
-    end
-
-    # @param [Money] object
-    def eql?(object)
-      @amount == object.instance_variable_get("@amount") &&
-        self.class.eql?(object.class)
-    end
+class BaseMoney
+  # @param [Integer] amount
+  def initialize(amount)
+    @amount = amount
   end
 
-  class Dollar < Money
-    # @param [Integer] multiplier
-    def times(multiplier)
-      Dollar.new(@amount * multiplier)
-    end
+  # @param [Integer] amount
+  def self.dollar(amount)
+    Dollar.new(amount)
   end
 
-  class Franc < Money
-    # @param [Integer] multiplier
-    def times(multiplier)
-      Franc.new(@amount * multiplier)
-    end
+  # @param [Integer] amount
+  def self.franc(amount)
+    Franc.new(amount)
+  end
+
+  # @param [BaseMoney] object
+  def eql?(object)
+    @amount == object.instance_variable_get("@amount") &&
+      self.class.eql?(object.class)
+  end
+end
+
+class Dollar < BaseMoney
+  # @param [Integer] multiplier
+  def times(multiplier)
+    Dollar.new(@amount * multiplier)
+  end
+end
+
+class Franc < BaseMoney
+  # @param [Integer] multiplier
+  def times(multiplier)
+    Franc.new(@amount * multiplier)
   end
 end
--- spec/dollar_spec.rb
+++ spec/dollar_spec.rb
@@ -1,22 +1,21 @@
-describe 'Money' do
+describe BaseMoney do
   it 'should test multiplication' do
-    five = Money::Dollar.new(5)
-    expect(Money::Dollar.new(10)).to eql(five.times(2))
-    expect(Money::Dollar.new(15)).to eql(five.times(3))
-    expect(Money::Dollar.new(10)).not_to eql(five.times(3))
+    five = BaseMoney.dollar(5)
+    expect(BaseMoney.dollar(10)).to eql(five.times(2))
+    expect(BaseMoney.dollar(15)).to eql(five.times(3))
   end
 
   it 'should test franc multiplication' do
-    five = Money::Franc.new(5)
-    expect(Money::Franc.new(10)).to eql(five.times(2))
-    expect(Money::Franc.new(15)).to eql(five.times(3))
+    five = BaseMoney.franc(5)
+    expect(BaseMoney.franc(10)).to eql(five.times(2))
+    expect(BaseMoney.franc(15)).to eql(five.times(3))
   end
 
   it 'should test equality' do
-    expect(Money::Dollar.new(5).eql?(Money::Dollar.new(5))).to be_truthy
-    expect(Money::Dollar.new(5).eql?(Money::Dollar.new(6))).to be_falsey
-    expect(Money::Franc.new(5).eql?(Money::Franc.new(5))).to be_truthy
-    expect(Money::Franc.new(5).eql?(Money::Franc.new(6))).to be_falsey
-    expect(Money::Franc.new(5).eql?(Money::Dollar.new(5))).to be_falsey
+    expect(BaseMoney.dollar(5).eql?(BaseMoney.dollar(5))).to be_truthy
+    expect(BaseMoney.dollar(5).eql?(BaseMoney.dollar(6))).to be_falsey
+    expect(BaseMoney.franc(5).eql?(BaseMoney.franc(5))).to be_truthy
+    expect(BaseMoney.franc(5).eql?(BaseMoney.franc(6))).to be_falsey
+    expect(BaseMoney.franc(5).eql?(BaseMoney.dollar(5))).to be_falsey
   end
 end
