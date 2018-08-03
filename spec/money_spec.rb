446afae2d1dbe807be17a93ca4e868ab0f321727
sozoev <sozoev@gmail.com>
2018-08-03 21:44:44 +0600

    Refactor money specs

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,14 +1,21 @@
 describe Money do
+  let(:bank) { Bank.new }
+  let(:five_francs) { Money.franc(5) }
+  let(:ten_francs) { Money.franc(10) }
+  let(:five_bucks) { Money.dollar(5) }
+
+  before do
+    bank.add_rate('CHF', 'USD', 2)
+  end
+
   it 'should test multiplication' do
-    five = Money.dollar(5)
-    expect(Money.dollar(10)).to eql(five.times(2))
-    expect(Money.dollar(15)).to eql(five.times(3))
+    expect(Money.dollar(10)).to eql(five_bucks.times(2))
+    expect(Money.dollar(15)).to eql(five_bucks.times(3))
   end
 
   it 'should test franc multiplication' do
-    five = Money.franc(5)
-    expect(Money.franc(10)).to eql(five.times(2))
-    expect(Money.franc(15)).to eql(five.times(3))
+    expect(Money.franc(10)).to eql(five_francs.times(2))
+    expect(Money.franc(15)).to eql(five_francs.times(3))
   end
 
   it 'should test equality' do
@@ -25,36 +32,29 @@ describe Money do
   end
 
   it 'should test simple addition' do
-    five = Money.dollar(5)
-    sum = five.plus(five)
-    bank = Bank.new
+    sum = five_bucks.plus(five_bucks)
     reduced = bank.reduce(sum, 'USD')
     expect(Money.dollar(10)).to eql(reduced)
   end
 
   it 'should test plus returns sum' do
-    five = Money.dollar(5)
-    sum = five.plus(five)
-    expect(five).to eql(sum.augend)
-    expect(five).to eql(sum.addend)
+    sum = five_bucks.plus(five_bucks)
+    expect(five_bucks).to eql(sum.augend)
+    expect(five_bucks).to eql(sum.addend)
   end
 
   it 'should reduce sum' do
     sum = Sum.new(Money.dollar(3), Money.dollar(4))
-    bank = Bank.new
     result = bank.reduce(sum, 'USD')
     expect(Money.dollar(7)).to eql(result)
   end
 
   it 'should reduce money' do
-    bank = Bank.new
     result = bank.reduce(Money.dollar(1), 'USD')
     expect(Money.dollar(1)).to eql(result)
   end
 
   it 'should reduce money in different currency' do
-    bank = Bank.new
-    bank.add_rate('CHF', 'USD', 2)
     result = bank.reduce(Money.franc(2), 'USD')
     expect(Money.dollar(1)).to eql(result)
   end
@@ -64,29 +64,17 @@ describe Money do
   end
 
   it 'should test mixed addition' do
-    five_bucks = Money.dollar(5)
-    ten_francs = Money.franc(10)
-    bank = Bank.new
-    bank.add_rate('CHF', 'USD', 2)
     result = bank.reduce(five_bucks.plus(ten_francs), 'USD')
     expect(Money.dollar(10)).to eql(result)
   end
 
   it 'should sum plus money' do
-    five_bucks = Money.dollar(5)
-    ten_francs = Money.franc(10)
-    bank = Bank.new
-    bank.add_rate('CHF', 'USD', 2)
     sum = Sum.new(five_bucks, ten_francs).plus(five_bucks)
     result = bank.reduce(sum, 'USD')
     expect(Money.dollar(15)).to eql(result)
   end
 
   it 'should sum times' do
-    five_bucks = Money.dollar(5)
-    ten_francs = Money.franc(10)
-    bank = Bank.new
-    bank.add_rate('CHF', 'USD', 2)
     sum = Sum.new(five_bucks, ten_francs).times(2)
     result = bank.reduce(sum, 'USD')
     expect(Money.dollar(20)).to eql(result)
